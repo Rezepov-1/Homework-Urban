9bf9c3299399bf7649e5c656db26ce7c318aaaf7
Rezepov-1 <timur_rezepov@mail.ru>
2024-06-06 01:06:51 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+num_homework = 12
+hours_spent = 1.5
+course_name = ('Python')
+time_per_task = hours_spent / num_homework
+print('Курс:' ,course_name+',' ' всего задач:',num_homework, ',' ' ' 'затрачено часов:',hours_spent, ',' ' среднее время выполнения',time_per_task, 'часа')
