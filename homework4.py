c2c7a5d7cfac02f90feef38682f1c92effa5285a
Тимур <timur_rezepov@mail.ru>
2024-06-06 19:08:48 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,11 @@
+my_string = input("Назови культурную столицу России: ")
+print("Длина строк:", len("Назови культурную столицу России: "))
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+
+first_char = my_string [0]
+print(first_char)
+last_char = my_string [-1]
+print (last_char)
