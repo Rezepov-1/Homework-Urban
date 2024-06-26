72dc9b0f596c03a024004d5bf8608d159bb8a113
Тимур <timur_rezepov@mail.ru>
2024-06-27 00:08:47 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,49 @@
+# Порой необходимо отслеживать, сколько раз вызывалась та или иная функция.
+# К сожалению, в Python не предусмотрен подсчёт вызовов автоматически.
+# Давайте реализуем данную фишку самостоятельно!
+#
+# Вам необходимо написать 3 функции:
+# Функция count_calls подсчитывающая вызовы остальных функций.
+# Функция string_info принимает аргумент - строку и возвращает кортеж из:
+# длины этой строки, строку в верхнем регистре, строку в нижнем регистре.
+# Функция is_contains принимает два аргумента: строку и список, и возвращает True,
+# если строка находится в этом списке, False - если отсутствует.
+# Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
+# Пункты задачи:
+# Создать переменную calls = 0 вне функций.
+# Создать функцию count_calls и изменять в ней значение переменной calls.
+# Эта функция должна вызываться в остальных двух функциях.
+# Создать функцию string_info с параметром string и реализовать логику работы по описанию.
+# Создать функцию is_contains с двумя параметрами string и list_to_search,
+# реализовать логику работы по описанию.
+# Вызвать соответствующие функции string_info и is_contains
+# произвольное кол-во раз с произвольными данными.
+# Вывести значение переменной calls на экран(в консоль).
+# print(string_info('Capybara'))
+# print(string_info('Armageddon'))
+# print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+# print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+# print(calls)
+
+calls = 0
+def cont_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    cont_calls()
+    return (len(string), string.lower(), string.upper())
+
+
+def is_contains(string, list_to_search):
+    cont_calls()
+    string_lower = string.lower()
+    list_to_search_lower = [item.lower() for item in list_to_search]
+    return string_lower in list_to_search_lower
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+
+print(calls)
