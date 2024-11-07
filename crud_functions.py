eed2b8dabca235d67aa0812cd479fda97619426f
Тимур <timur_rezepov@mail.ru>
2024-11-07 19:48:43 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -1,30 +1,62 @@
 import sqlite3
 
 
-# Функция для инициализации базы данных и создания таблицы Products
+# Функция для создания таблиц Products и Users, если они ещё не созданы
 def initiate_db():
-    connection = sqlite3.connect('not_telegram.db')
-    cursor = connection.cursor()
+    conn = sqlite3.connect('not_telegram.db')
+    cursor = conn.cursor()
 
+    # Создание таблицы Products
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS Products (
-            id INTEGER PRIMARY KEY,
-            title TEXT NOT NULL,
-            description TEXT,
-            price INTEGER NOT NULL
-        )
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+    )
     ''')
-    connection.commit()
-    connection.close()
+
+    # Создание таблицы Users
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL DEFAULT 1000,
+    UNIQUE(username, email)  -- Ограничение уникальности на комбинацию username + email
+    )
+    ''')
+    conn.commit()
+    conn.close()
 
 
 # Функция для получения всех продуктов из таблицы Products
 def get_all_products():
-    connection = sqlite3.connect('not_telegram.db')
-    cursor = connection.cursor()
-
+    conn = sqlite3.connect('not_telegram.db')
+    cursor = conn.cursor()
     cursor.execute("SELECT * FROM Products")
     products = cursor.fetchall()
-
-    connection.close()
+    conn.close()
     return products
+
+
+# Функция для добавления пользователя в таблицу Users
+def add_user(username, email, age):
+    conn = sqlite3.connect('not_telegram.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (username, email, age, 1000))
+    conn.commit()
+    conn.close()
+
+
+# Функция для проверки наличия пользователя в таблице Users
+def is_included(username, email):
+    conn = sqlite3.connect('not_telegram.db')
+    cursor = conn.cursor()
+    # Проверяем, существует ли пользователь с данным именем и email
+    cursor.execute('SELECT * FROM Users WHERE username = ? AND email = ?', (username, email))
+    result = cursor.fetchone()
+    conn.close()
+    return result is not None
